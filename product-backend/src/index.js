4cd9470299f08bf5e9c90bb2912fe0a3aad31a31
Deepraj-devikar <deeprajdevikar19@gmail.com>
2026-03-27 13:34:23 +0530

    DEEPRAJ DEVIKAR - ADD: product backend structure

---
+++ product-backend/src/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const app = express();
+const host = process.env.APP_HOST;
+const port = process.env.APP_PORT;
+const api_version = process.env.API_VERSION;
+
+app.use(cors());
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+  logger.info(`Server started at ${host}:${port}/api/${api_version}/`);
+});
+
+export default app;
