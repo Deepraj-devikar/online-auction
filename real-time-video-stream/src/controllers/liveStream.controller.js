dfbbeab05b22138413c7baf99b09af680caa6d78
Deepraj-devikar <deeprajdevikar19@gmail.com>
2026-03-27 13:14:19 +0530

    DEEPRAJ DEVIKAR - ADD: real time video stream controller

---
+++ real-time-video-stream/src/controllers/liveStream.controller.js
@@ -0,0 +1,15 @@
+import HttpStatus from 'http-status-codes';
+
+/**
+ * Controller to create a new liveStream
+ * @param  {object} req - request object
+ * @param {object} res - response object
+ * @param {Function} next
+ */
+export const setRTMPServer = async (req, res, next) => {
+
+};
+
+export const currentTime = async (req, res, next) => {
+
+}
--- real-time-video-stream/src/routes/liveStream.route.js
+++ real-time-video-stream/src/routes/liveStream.route.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import { currentTime, setRTMPServer } from '../controllers/liveStream.controller';
+import { userAuth } from '../middlewares/auth.middleware';
 
 const router = express.Router();
+
+//route to create a new liveStream
+router.post('', userAuth, setRTMPServer);
+
+//route to show current time
+router.get('/current-time', currentTime);
 
 export default router;
