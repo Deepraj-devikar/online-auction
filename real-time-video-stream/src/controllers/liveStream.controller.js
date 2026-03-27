e4f8c8f531a8a8a7e7b34957870be932cac8f282
Deepraj-devikar <deeprajdevikar19@gmail.com>
2026-03-27 13:18:59 +0530

    DEEPRAJ DEVIKAR - REFACTOR: real time video stream controller

--- real-time-video-stream/src/controllers/liveStream.controller.js
+++ real-time-video-stream/src/controllers/liveStream.controller.js
@@ -1,4 +1,5 @@
 import HttpStatus from 'http-status-codes';
+import { runNodeMediaServer, stopNodeMediaServer } from '../utils/liveStream.util';
 
 /**
  * Controller to create a new liveStream
@@ -7,9 +8,40 @@ import HttpStatus from 'http-status-codes';
  * @param {Function} next
  */
 export const setRTMPServer = async (req, res, next) => {
+  try {
+    const startDuration = req.body.startDuration.split(":");
+    let startDurationMS = (parseInt(startDuration[0]) * 60 + parseInt(startDuration[1])) * 60 * 1000;
+    startDurationMS = startDurationMS > 0 ? startDurationMS : 0;
 
+    const liveStreamDuration = req.body.liveStreamDuration.split(":");
+    const liveStreamDurationMS = (parseInt(liveStreamDuration[0]) * 60 + parseInt(liveStreamDuration[1])) * 60 * 1000;
+
+    // will run node media server at start time
+    runNodeMediaServer(startDurationMS, req.body.appName)
+    .then(nodeMediaServer => {
+      stopNodeMediaServer(nodeMediaServer, liveStreamDurationMS);
+    })
+    .catch(error => {
+      // node media server is not running
+      next(error);
+    });
+
+    const currentDateTime = new Date();
+    res.status(HttpStatus.CREATED).json({
+      code: HttpStatus.CREATED,
+      message: 'LiveStream created successfully',
+      startDateTime: new Date(currentDateTime.getTime() + startDurationMS),
+      endDateTime: new Date(currentDateTime.getTime() + startDurationMS + liveStreamDurationMS)
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const currentTime = async (req, res, next) => {
-
+  res.status(HttpStatus.OK).json({
+    code: HttpStatus.OK,
+    currentDateTime: new Date(),
+    message: 'current time'
+  })
 }
